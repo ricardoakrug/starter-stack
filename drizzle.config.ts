cbf69fa2daf728f05452d1138b982108055e2dfc
ricardoakrug <ricardo@cena23.com.br>
2025-03-19 14:51:39 -0300

    Added Drizzle

---
+++ drizzle.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from "drizzle-kit";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not set");
+}
+
+export default {
+  schema: "./src/lib/db/schema/*",
+  out: "./drizzle",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL,
+  },
+  verbose: true,
+  strict: true,
+} satisfies Config;
